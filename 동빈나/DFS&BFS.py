c0875ce61b7a930083151f9cc9b0e8f15315567b
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2021-09-27 02:57:50 +0000

    dfs/bfs 예제 미로탈출(진행중)

--- 동빈나/DFS&BFS.py
+++ 동빈나/DFS&BFS.py
@@ -61,3 +61,64 @@ def bfs(graph, start, visited):
 
 bfs(graph,1, visited) 
 
+## 음료수 얼려 먹기
+# 특정 지점의 주변 상,하,좌,우를 살펴본 뒤에 주변 지점 중에서 값이 '0'이면서 아직 방문하지 않은 지점이 있다면 해당 지점을 방문합니다.
+# 2. 방문한 지점에서 주변 상,하,좌,우를 살펴본 뒤에 주변 지점 중에서 값이 '0'이면서 아직 방문하지 않은 지점이 있다면 해당 지점을 방문합니다.
+# 3. 모든 노드에 대하여 1~2번 과정을 반복하여, 방문하지 앟은 지점의 수를 카운트합니다.
+
+#DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+def dfs(x,y):
+  # 주어진 범위를 벗어나는 경우에는 즉시 종료
+  if x <= -1 or x>=  n or y <= -1 or y >= m:
+    return False
+  
+  # 현재 노드를 아직 방문하지 않았다면
+  if graph[x][y] ==0:
+    # 해당 노드 방문 처리
+    graph[x][y] = 1
+    # 상, 하, 좌, 우 위치들도 모두 재귀적으로 호출- 방문처리 하기 위함
+    dfs(x-1, y)
+    dfs(x, y-1)
+    dfs(x+1, y)
+    dfs(x, y+1)
+    return True
+  return False
+
+# N,M을 공백 기준으로 구분하여 입력 받기
+n , m = map(int, input().split())
+
+# 2차원 리스트의 맵 정보 입력 받기 
+graph = []
+for i in range(n):
+  graph.append(list(map(int,input())))
+
+# 모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+  for j in range(m):
+    # 현재 위치에서 DFS 수행
+    if dfs(i, j) == True:
+      result += 1
+
+print(result)
+
+
+
+## 미로탈출
+n , m = map(int, input().split())
+
+graph = []
+for _ in range(n):
+  graph.append(list(map(int,input())))
+
+result = 0
+# for i in range(n):
+#   for j in range(m):
+
+from collections import deque
+
+def bfs(x,y):
+  
+
+
+
