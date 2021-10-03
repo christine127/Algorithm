4bd4216b3fe7bac38e42354100bf2a0f4bf2fab0
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2021-10-03 13:11:48 +0000

    dfs/bfs 미로탈출

--- 동빈나/DFS&BFS.py
+++ 동빈나/DFS&BFS.py
@@ -105,20 +105,41 @@ print(result)
 
 
 ## 미로탈출
-n , m = map(int, input().split())
+n, m = map(int,input().split())
 
 graph = []
-for _ in range(n):
+for i in range(n):
   graph.append(list(map(int,input())))
-
-result = 0
-# for i in range(n):
-#   for j in range(m):
 
 from collections import deque
 
 def bfs(x,y):
-  
+  # 큐
+  queue = deque()
+  queue.append((x,y))
 
+  while queue:
+    x,y = queue.popleft()
+    # 현재 위치에서 4가지 방향으로의 위치확인
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      # 미로 찾기 공간을 벗어난 경우 무시
+      if nx < 0 or nx >= n or ny < 0 or ny >= m :
+        continue
+      # 벽인 경우 무시
+      if graph[nx][ny] == 0:
+        continue
+      # 해당 노드를 처음 방문하는 경웅만 최단 거리 기록
+      if graph[nx][ny] == 1:
+        graph[nx][ny] = graph[x][y] + 1
+        queue.append((nx,ny))
+  return graph[n-1][m-1]
+
+  # 이동할 네 가지 방향 정의 (상,하,좌,우)
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+print(bfs(0,0))
 
 
