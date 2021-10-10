36d364b04e5a6d59ad811f184274d14158b43a35
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2021-10-10 16:53:07 +0000

    재귀함수 2개

--- 백준/그리디알고리즘.py
+++ 백준/그리디알고리즘.py
@@ -14,3 +14,22 @@ while remainder >0:
   i +=1 
 
 print(count)
+
+#회의실 배정 
+N = int(input())
+meeting = []
+for _ in range(N):
+  st, en = map(int, input().split(' '))
+  meeting.append((st,en,en-st))#시작, 끝, 회의시간
+  
+meeting.sort(key = lambda x: (x[0],x[2]))
+print(meeting)
+# count = []
+# for meet in meeting:
+#   lst = []
+#   lst.append(meet)
+#   for another_meet in meeting:
+#     if  
+#   count.append(len(lst))  
+  
+# print(max(count))
--- 백준/재귀함수.py
+++ 백준/재귀함수.py
@@ -7,3 +7,46 @@ def factorial(n):
 
 print(factorial(int(input())))
 
+#피보나치
+def Fibonacci(n):
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1  
+  else:
+    return Fibonacci(n-1)+Fibonacci(n-2)
+
+print(Fibonacci(int(input())))
+
+#별찍기
+def star(n):
+  global Map
+
+  if n == 3:
+    Map[0][:3] = [1]*3
+    Map[1][:3] = [1,0,1]
+    Map[2][:3] = [1]*3 
+    return
+
+  a = n//3
+  star(a)
+  for i in range(3):
+    for j in range(3):
+      if i == 1 and j == 1:
+        continue
+      for k in range(a):
+        Map[a*i+k][a*j:a*(j+1)] = Map[k][:a]
+
+N= int(input())
+Map = [[0]*N  for _ in range(N)]
+star(N)
+
+for i in Map:
+  for j in i:
+    if j == 1:
+      print('*', end = '')
+    else:
+      print(' ', end= '')
+  print()
+
+
