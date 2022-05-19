a38479de36662a73df99921534afab27612ec2e7
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2022-05-19 03:57:08 +0000

    퀵정렬

--- 동빈나/정렬알고리즘.py
+++ 동빈나/정렬알고리즘.py
@@ -1,5 +1,5 @@
 #선택 정렬
-array = [7,5,9,0,3,1,6,2,4,8]
+array = [5,7,9,0,3,1,6,2,4,8]
 for i in range(len(array)):
     min_index = i #가장 작은 원소의 인덱스
     for j in range(i+1, len(array)):
@@ -21,3 +21,44 @@ print(array)
 
 
 #퀵 정렬: 기준 데이터를 설정하고 그 기준보다 큰 데이터와 작은 데이터의 위치를 바꾸는 방법
+#시간 복잡도: 평균 O(NlogN) 최악의 경우 O(N^2), 첫 번째 원소를 피벗으로 삼을 때, 이미 정렬된 배열에 대해서 퀵 정렬 수행 시,O(N^2)
+#표준라이브러리는 기본적으로 NlogN 유지
+
+def quick_sort(array, start,end):
+    if start>= end:
+        return
+    pivot = start
+    left = start+1
+    right = end
+    while (left <= right):
+        #피벗보다 큰 데이터를 찾을 때까지 반복
+        while(left <=  end and array[left] <= array[pivot]):
+            left +=1
+        #피벗보다 작은 데이터를 찾을 때까지 반복
+        while(right > start and array[right] >= array[pivot]):
+            right -=1
+        if(left>right): # 엇갈렸다면 작은 데이터와 피벗을 교체
+            array[right], array[pivot] = array[pivot], array[right]
+        else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            array[left], array[right] = array[right], array[left]
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quick_sort(array, start, right-1)
+    quick_sort(array, right+1, end)
+quick_sort(array, 0, len(array)-1)
+
+print(array)
+
+def quick_sort(array):
+    # 리스트가 하나 이하의 원소만을 담고 있다면 종료
+    if len(array) <= 1:
+        return array
+    pivot = array[0] #피벗은 첫 번째 원소
+    tail = array[1:] #피벗을 제외한 리스트
+
+    left_side = [x for x in tail if x <= pivot] # 분할된 왼쪽 부분
+    right_side = [x for x in tail if x > pivot] # 분할된 오른쪽 부분
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행하고, 전체 리스트 반환
+    return quick_sort(left_side) +[pivot] + quick_sort(right_side)
+print(quick_sort(array))
+
