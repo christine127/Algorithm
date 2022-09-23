04c359088a64742a68587da25b765fcaa89e5c30
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2022-09-24 06:51:10 +0900

    Update Consecutive_Numbers.sql
    윈도우 함수 사용 버전

--- LeetCode/Consecutive_Numbers.sql
+++ LeetCode/Consecutive_Numbers.sql
@@ -6,3 +6,15 @@ FROM
     INNER JOIN Logs AS L3 ON L1.ID+2 = L3.ID
 WHERE L1.num = L2.num
 AND L2.num = L3.num
+
+-- lead 사용(window 함수) 
+SELECT DISTINCT T.num AS ConsecutiveNums
+FROM
+    (SELECT
+        num,
+        Lead(num,1) OVER (ORDER BY Id) AS 'lead1',
+        Lead(num,2) OVER (ORDER BY Id) AS 'lead2'
+    FROM Logs) AS T
+WHERE 
+    T.num = T.lead1 
+    AND T.lead1 = T.lead2
