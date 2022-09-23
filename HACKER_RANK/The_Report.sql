8e99a72155f535bfbb3642d7d2d005431962fff7
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2022-09-23 11:27:51 +0900

    Update The_Report.sql
    선생님답안 Between 이용해서 풀었음.

--- HACKER_RANK/The_Report.sql
+++ HACKER_RANK/The_Report.sql
@@ -1,3 +1,4 @@
+-- 나의 답안
 SELECT 
     CASE WHEN 8 <= g.grade THEN s.name
     END AS Name,
@@ -7,6 +8,20 @@ FROM
     students AS s LEFT JOIN grades as g 
     ON (CASE WHEN s.marks = 100 THEN g.grade = 10
              ELSE TRUNCATE(s.marks, -1) = g.min_mark END)
+ORDER BY 
+    g.grade DESC,
+    s.name ASC,
+    s.marks ASC
+
+-- 정답(인프런 백문이 불여일타)
+SELECT 
+    CASE WHEN 8 <= g.grade THEN s.name
+    END AS Name,
+    g.grade,
+    s.marks 
+FROM 
+    students AS s INNER JOIN grades as g 
+    ON s.marks BETWEEN g.min_mark AND g.max_mark
 ORDER BY 
     g.grade DESC,
     s.name ASC,
