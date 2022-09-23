b6ae98ce582294ee61fa06d13074d401a31808a0
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2022-09-24 07:09:59 +0900

    Update 184_Department Highest Salary.sql
    윈도우함수로 풀어본 버전

--- LeetCode/184_Department Highest Salary.sql
+++ LeetCode/184_Department Highest Salary.sql
@@ -1,3 +1,4 @@
+-- 서브쿼리 
 SELECT D.name AS Department, E.name AS Employee, E.salary
 FROM Employee E, 
      Department D
@@ -5,3 +6,19 @@ WHERE (E.departmentId, E.salary) in (SELECT departmentId, MAX(Salary)
                                     FROM Employee
                                     GROUP BY departmentId) 
 AND E.departmentId = D.id
+
+
+-- 윈도우함수(나의 답안)
+SELECT
+    Department, 
+    Employee,
+    MaxSalary AS Salary
+FROM
+    (SELECT 
+        D.name AS Department, 
+        E.name AS Employee,
+        E.Salary,
+        MAX(E.salary) OVER (Partition BY departmentId) AS MaxSalary
+    FROM 
+        Employee AS E INNER JOIN Department AS D ON E.departmentId = D.id) AS T
+WHERE T.Salary = MaxSalary
