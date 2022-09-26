5168cbe7d83fb4118e4ce48ae50785201daecf28
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2022-09-26 16:58:15 +0900

    Update 온라인 쇼핑몰의 Stickiness.sql

--- solvesql/온라인 쇼핑몰의 Stickiness.sql
+++ solvesql/온라인 쇼핑몰의 Stickiness.sql
@@ -1,3 +1,4 @@
+-- SELECT 서브쿼리 (나의 답안)
 SELECT 
   order_date AS dt,
   dau,
@@ -20,3 +21,19 @@ FROM
   GROUP BY order_date
   HAVING DAU > 0 
   ) AS T
+
+-- LEFT JOIN (강사님)
+SELECT 
+  dt,
+  dau,
+  wau,
+  ROUND(dau/wau,2) AS stickiness
+FROM
+  (SELECT 
+    d.order_date AS dt,
+    COUNT(DISTINCT d.customer_id) AS dau,
+    COUNT(DISTINCT w.customer_id) AS wau
+  FROM records AS d
+    LEFT JOIN records AS w ON w.order_date BETWEEN DATE_ADD(d.order_date, INTERVAL -6 DAY) AND d.order_date
+  WHERE d.order_date BETWEEN '2020-11-01' AND '2020-11-30'
+  GROUP BY d.order_date) AS T
