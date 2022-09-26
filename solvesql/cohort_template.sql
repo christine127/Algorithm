0068a4fef34f1faa178a0b63dfc8a0e7968398ce
Hyunji Lee <76949239+christine127@users.noreply.github.com>
2022-09-26 17:02:09 +0900

    Update cohort_template.sql

--- solvesql/cohort_template.sql
+++ solvesql/cohort_template.sql
@@ -1,3 +1,4 @@
+-- 클래식 리텐션
 WITH
   records_preprocessed AS (
     SELECT
@@ -29,3 +30,27 @@ WITH
       records_preprocessed
     GROUP BY
       first_order_month
+
+-- 롤링 리텐션(마지막 달까지 유지한 것으로 계산)
+WITH customers AS ( 
+     SELECT customer_id
+          , DATE_FORMAT(first_order_date, '%Y-%m-01') first_order_month
+          , DATE_FORMAT(last_order_date, '%Y-%m-01') last_order_month
+     FROM customer_stats c
+)
+
+SELECT first_order_month
+     , COUNT(DISTINCT customer_id) month0
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 1 MONTH) <= last_order_month THEN customer_id END) AS month1
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 2 MONTH) <= last_order_month THEN customer_id END) AS month2
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 3 MONTH) <= last_order_month THEN customer_id END) AS month3
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 4 MONTH) <= last_order_month THEN customer_id END) AS month4
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 5 MONTH) <= last_order_month THEN customer_id END) AS month5
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 6 MONTH) <= last_order_month THEN customer_id END) AS month6
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 7 MONTH) <= last_order_month THEN customer_id END) AS month7
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 8 MONTH) <= last_order_month THEN customer_id END) AS month8
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 9 MONTH) <= last_order_month THEN customer_id END) AS month9
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 10 MONTH) <= last_order_month THEN customer_id END) AS month10
+     , COUNT(DISTINCT CASE WHEN DATE_ADD(first_order_month, INTERVAL 11 MONTH) <= last_order_month THEN customer_id END) AS month11
+FROM customers
+GROUP BY 1
